a2c6cab2d6ed5b6859d765373e8e14189e48e86b
richlaji <494846236@qq.com>
2016-05-30 13:31:20 +0800

    four icon

--- ContractCouple/MainViewController.h
+++ ContractCouple/MainViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainViewController.h
 //  ContractCouple
 //
 //  Created by 张泽昕 on 16/5/30.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MainViewController : UITabBarController
 
 @end
-
